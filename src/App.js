f6f07bc02c8a835286b09e2d90cd016f245a36ed
KIB KIB <pedrooaulicino@gmail.com>
2020-03-11 02:34:16 -0300

    React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "./config/Reactotronconfig";
 
+import GlobalStyle from "./style/global";
+import Header from "./components/Header";
+
+import history from "./services/history";
+
+import Routes from "./routes";
+import store from "./store";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router history={history}>
+        <Header></Header>
+        <GlobalStyle />
+        <ToastContainer autoClose={3000} />
+        <Routes />
+      </Router>
+    </Provider>
   );
 }
-
 export default App;
